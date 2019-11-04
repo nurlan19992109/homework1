12be954f87ee9a6ca9ea5ac3e435b09ceade7d66
nurlan99 <n.u.rl.a.n.nq@gmail.com>
2019-11-05 01:34:37 +0400

    Mistakes fixed

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-                System.out.println("Let the game begin!");
-                Random random = new Random();
-                int x = random.nextInt(100);
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please enter your name");
-                String username = scanner.nextLine();
-                System.out.println("Please enter the number!");
+        System.out.println("Let the game begin!");
+        Random random = new Random();
+        int x = random.nextInt(100);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String username = scanner.nextLine();
+        System.out.println("Please enter the number!");
 
 
-                do {
-                    int entrdnmbr = scanner.nextInt();
+        do {
+            int input = scanner.nextInt();
 
-                    if (entrdnmbr > x) {
-                        System.out.println("Your number is too big. Please, try again.");
-                    }
+            if (input > x) {
+                System.out.println("Your number is too big. Please, try again.");
+            }
 
-                    if (entrdnmbr < x) {
-                        System.out.println("Your number is too small. Enter another number.");
-                    }
-                    if (entrdnmbr == x) {
-                        System.out.println("Congrtultions " + username + "!");
-
-                    }
-
-                }
-                while (true);
-
+            if (input < x) {
+                System.out.println("Your number is too small. Enter another number.");
+            }
+            if (input == x) {
+                System.out.println("Congrtultions " + username + "!");
+                break;
             }
 
         }
+        while (true);
+
+    }
+
+}
